59825271a3a11cad4bbd8cf5251a989e6ed2ab63
Jhon Binag <paulbinag1963@gmail.com>
2025-04-07 13:40:10 +0800

    update

---
+++ backend/services/flodesk/segments/segments.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const segments = () => {
+  return (
+    <div>segments</div>
+  )
+}
+
+export default segments
---
+++ backend/services/flodesk/subscribers/subscribers.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const subscribers = () => {
+  return (
+    <div>subscribers</div>
+  )
+}
+
+export default subscribers
